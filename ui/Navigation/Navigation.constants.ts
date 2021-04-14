d07f750d383060805febd873362b4cc0060bd1c4
petergrillot <petergrillot@gmail.com>
2021-04-14 17:23:01 -0400

    Add image link

--- ui/Navigation/Navigation.constants.ts
+++ ui/Navigation/Navigation.constants.ts
@@ -1,44 +1,44 @@
 export type NavigationType = {
-	href: string;
-	icon: string;
-	text: string;
+  href: string;
+  icon: string;
+  text: string;
 };
 
 export const NAVIGATION = [
-	{
-		href: '//instagram.com/curse_words',
-		icon: 'fab fa-instagram',
-		text: 'Instagram'
-	},
-	{
-		href: '//twitter.com/CurseWordsBand',
-		icon: 'far fa-trash-alt',
-		text: 'Twitter'
-	},
-	{
-		href: '//www.facebook.com/DontSayCurseWords/',
-		icon: 'fab fa-facebook',
-		text: 'Facebook'
-	},
-	{
-		href: '//dontsaycursewords.bandcamp.com/',
-		icon: 'fab fa-bandcamp',
-		text: 'Bandcamp'
-	},
-	{
-		href: '//open.spotify.com/artist/5Yd3VFwLLJaSAPQRKwYXSN',
-		icon: 'fab fa-spotify',
-		text: 'Spotify'
-	},
-	{
-		href: '//www.youtube.com/channel/UCAi2C7uk5a-gNXyEfqXQMRg',
-		icon: 'fab fa-youtube',
-		text: 'YouTube'
-	},
-	{
-		// eslint-disable-next-line
-		href: 'mailto:dontsaycursewords@gmail.com',
-		icon: 'far fa-envelope',
-		text: 'Email'
-	}
+  {
+    href: '//instagram.com/curse_words',
+    icon: 'fab fa-instagram',
+    text: 'Instagram'
+  },
+  {
+    href: '//twitter.com/CurseWordsBand',
+    icon: 'far fa-trash-alt',
+    text: 'Twitter'
+  },
+  {
+    href: '//www.facebook.com/DontSayCurseWords/',
+    icon: 'fab fa-facebook',
+    text: 'Facebook'
+  },
+  {
+    href: '//dontsaycursewords.bandcamp.com/',
+    icon: 'fab fa-bandcamp',
+    text: 'Bandcamp'
+  },
+  {
+    href: '//open.spotify.com/artist/5Yd3VFwLLJaSAPQRKwYXSN',
+    icon: 'fab fa-spotify',
+    text: 'Spotify'
+  },
+  {
+    href: '//www.youtube.com/channel/UCAi2C7uk5a-gNXyEfqXQMRg',
+    icon: 'fab fa-youtube',
+    text: 'YouTube'
+  },
+  {
+    // eslint-disable-next-line
+    href: 'mailto:dontsaycursewords@gmail.com',
+    icon: 'far fa-envelope',
+    text: 'Email'
+  }
 ];
